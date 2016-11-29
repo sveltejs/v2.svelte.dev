68d5266732ea7acb3a929761cd13a33d6a854a1b
Rich Harris <richard.a.harris@gmail.com>
2016-11-29 16:45:04 -0500

    show message if svelte is unsupported

--- src/repl/main.js
+++ src/repl/main.js
@@ -1,12 +1,16 @@
 import Repl from './Repl.html';
 import examples from './examples.js';
 
-const match = /gist=(.+)$/.exec( window.location.search );
-const gist = match ? match[1] : examples[0].gist;
+if ( window.svelte ) {
+	const match = /gist=(.+)$/.exec( window.location.search );
+	const gist = match ? match[1] : examples[0].gist;
 
-const repl = new Repl({
-	target: document.querySelector( 'main' ),
-	data: { gist }
-});
+	const repl = new Repl({
+		target: document.querySelector( 'main' ),
+		data: { gist }
+	});
 
-window.repl = repl;
+	window.repl = repl;
+} else {
+	document.querySelector( 'main' ).innerHTML = `<p style='text-align: center; margin: 0; padding: 4em 3em 8em 3em; line-height: 1.5;'>Svelte generates components that work in all modern JavaScript environments, but the Svelte compiler only runs in Node 6+ and browsers that support ES2015 features. Please reopen this page in a different browser such as Chrome.</p>`;
+}
