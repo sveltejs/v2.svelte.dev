1b228e1627e27e92d670ead4584a9d447961310e
Rich Harris <richard.a.harris@gmail.com>
2016-11-29 11:43:58 -0500

    add a couple more examples

--- src/repl/examples.js
+++ src/repl/examples.js
@@ -9,5 +9,23 @@ export default [
 		// https://gist.github.com/Rich-Harris/0a4d409dcea5883c47167a55965f03d9
 		title: 'If blocks',
 		gist: '0a4d409dcea5883c47167a55965f03d9'
+	},
+
+	{
+		// https://gist.github.com/Rich-Harris/51b8e73f79823f1cee2658d4b04f609b
+		title: 'Each blocks',
+		gist: '51b8e73f79823f1cee2658d4b04f609b'
+	},
+
+	{
+		// https://gist.github.com/Rich-Harris/34a038757e4d36140fe450d61da6b564
+		title: 'Scoped styles',
+		gist: '34a038757e4d36140fe450d61da6b564'
+	},
+
+	{
+		// https://gist.github.com/Rich-Harris/44e20b4e0224617d228e3c38c119ee76
+		title: 'SVG Clock',
+		gist: '44e20b4e0224617d228e3c38c119ee76'
 	}
 ]
