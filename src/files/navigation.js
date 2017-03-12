c971b63c3cc2b070643f51675f262f518c86325f
William Esz <flagello@users.noreply.github.com>
2017-03-12 02:09:54 +0100

    Add ability to close sidebar by clicking outside

--- src/files/navigation.js
+++ src/files/navigation.js
@@ -2,15 +2,18 @@ var sidebarOpen = false;
 var menuText = ["Menu", "Close"];
 var menuElement = document.querySelector(".menu-link");
 var sidebarElement = document.querySelector(".sidebar");
+var mainElement = document.querySelector("main");
 var headerElement = document.querySelector("header");
 var rAF;
 
+mainElement.addEventListener( "click", toggleSidebar, true );
 menuElement.addEventListener( "click", toggleSidebar, false );
 window.addEventListener( "hashchange", toggleSidebar, false );
 
 function toggleSidebar ( event ) {
     // Don't toggle when there's nothing to hide.
     if (event.type === "hashchange" && !sidebarOpen) return;
+    if (event.currentTarget === mainElement && !sidebarOpen) return;
 
     sidebarOpen = (!sidebarOpen);
 
