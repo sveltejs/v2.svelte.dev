ecdcdf328777d67063d37579e7923782cc580fc5
Rich-Harris <richard.a.harris@gmail.com>
2017-03-19 15:25:43 -0400

    remove unused files

--- src/repl/version.js
+++
@@ -1,8 +0,0 @@
-const match = /version=([^\&]+)/.exec( window.location.search );
-
-export const version = ( typeof svelte !== 'undefined' && svelte.VERSION ) ?
-	svelte.VERSION :
-	match ? match[1] :
-	null;
-
-console.log( `running Svelte compiler version %c${version}`, 'font-weight: bold' ); // eslint-disable-line no-console
