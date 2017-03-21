541f4afcbe004e910a2ac8bef328f5ef776126ac
Rich-Harris <richard.a.harris@gmail.com>
2017-03-21 10:23:34 -0400

    remove built examples from repo

--- shared/routes/Repl/examples.js
+++
@@ -1,2 +0,0 @@
-// this file is auto-generated, don't edit it
-export default [{"id":"hello-world","title":"Hello World!"},{"id":"if-blocks","title":"If blocks"},{"id":"each-blocks","title":"Each blocks"},{"id":"scoped-styles","title":"Scoped styles"},{"id":"svg-clock","title":"SVG Clock"},{"id":"line-chart","title":"Line chart"},{"id":"area-chart","title":"Area chart"},{"id":"bar-chart","title":"Bar chart"},{"id":"self-references","title":"Self-references"}];
