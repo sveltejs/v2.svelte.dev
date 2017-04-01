8e671e93e42bb7abb49b703243d2655f18596258
Rich-Harris <richard.a.harris@gmail.com>
2017-04-01 14:53:41 -0400

    bundle components with Rollup, load dependencies from packd.now.sh

--- scripts/prep/build-repl.js
+++ scripts/prep/build-repl.js
@@ -1,4 +1,4 @@
 const { copy } = require( './utils.js' );
 
 copy( `node_modules/codemirror/lib/codemirror.css`, `public/codemirror.css` );
-copy( `node_modules/magic-string/dist/magic-string.umd.js`, `public/magic-string.umd.js` );
+copy( `node_modules/rollup/dist/rollup.browser.js`, `public/rollup.browser.js` );
