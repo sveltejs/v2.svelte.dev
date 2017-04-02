f8d966f2495e5de70c2c1fc4a4f874c88b0aacc3
Rich-Harris <richard.a.harris@gmail.com>
2017-04-02 15:35:23 -0400

    group examples into categories

--- scripts/prep/build-examples.js
+++ scripts/prep/build-examples.js
@@ -9,36 +9,47 @@ const root = path.resolve( __dirname, '../..' );
 const manifest = require( `${root}/examples/manifest.json` );
 const examples = fs.readdirSync( `${root}/examples` ).filter( file => file[0] !== '.' && file !== 'manifest.json' );
 
-assert.deepEqual( examples.sort(), manifest.slice().sort() );
+// check all examples exist and are used
+const flattened = manifest.reduce( ( f, group ) => ( f.push( ...group.examples ), f ), [] );
+assert.deepEqual( examples.sort(), flattened.slice().sort() );
 
 mkdirp( `${root}/public/examples` );
 
 const summary = [];
-manifest.forEach( id => {
-	const example = require( `${root}/examples/${id}/example.json` );
+manifest.forEach( group => {
+	const groupSummary = [];
 
-	example.data = example.data || {};
+	group.examples.forEach( id => {
+		const example = require( `${root}/examples/${id}/example.json` );
 
-	example.components = glob.sync( '**/*.html', { cwd: `${root}/examples/${id}` })
-		.map( file => {
-			return {
-				name: file.replace( /\.html$/, '' ),
-				entry: file === 'App.html' ? true : undefined,
-				source: fs.readFileSync( `${root}/examples/${id}/${file}`, 'utf-8' )
-			};
-		})
-		.sort( ( a, b ) => {
-			if ( a.name === 'App' ) return -1;
-			if ( b.name === 'App' ) return 1;
+		example.data = example.data || {};
 
-			return a.name < b.name ? -1 : 1;
+		example.components = glob.sync( '**/*.html', { cwd: `${root}/examples/${id}` })
+			.map( file => {
+				return {
+					name: file.replace( /\.html$/, '' ),
+					entry: file === 'App.html' ? true : undefined,
+					source: fs.readFileSync( `${root}/examples/${id}/${file}`, 'utf-8' )
+				};
+			})
+			.sort( ( a, b ) => {
+				if ( a.name === 'App' ) return -1;
+				if ( b.name === 'App' ) return 1;
+
+				return a.name < b.name ? -1 : 1;
+			});
+
+		fs.writeFileSync( `${root}/public/examples/${id}.json`, JSON.stringify( example ) );
+
+		groupSummary.push({
+			id,
+			title: example.title
 		});
-
-	fs.writeFileSync( `${root}/public/examples/${id}.json`, JSON.stringify( example ) );
+	});
 
 	summary.push({
-		id,
-		title: example.title
+		name: group.name,
+		examples: groupSummary
 	});
 });
 
