503fd64ebde8416632c81708e8de5e6fe2f6c5ec
Rich-Harris <richard.a.harris@gmail.com>
2017-04-12 23:44:04 -0400

    trace errors back to their origin, if possible

--- scripts/prep/build-repl.js
+++ scripts/prep/build-repl.js
@@ -2,3 +2,4 @@ const { copy } = require( './utils.js' );
 
 copy( `node_modules/codemirror/lib/codemirror.css`, `public/codemirror.css` );
 copy( `node_modules/rollup/dist/rollup.browser.js`, `public/rollup.browser.js` );
+copy( `node_modules/svelte/compiler/svelte.js`, `public/svelte.js` );
