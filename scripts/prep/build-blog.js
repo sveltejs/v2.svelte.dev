f30bf1432d2792dd6ed03756cfb9ac3084720432
Rich Harris <richard.a.harris@gmail.com>
2017-08-07 11:04:16 -0400

    add getting started blog post

--- scripts/prep/build-blog.js
+++ scripts/prep/build-blog.js
@@ -5,32 +5,36 @@ const marked = require( 'marked' );
 
 const root = path.resolve( __dirname, '../..' );
 
-const posts = fs.readdirSync( `${root}/blog` ).map( file => {
-	if ( file[0] === '.' || path.extname( file ) !== '.md' ) return;
+const posts = fs.readdirSync( `${root}/blog` )
+	.map( file => {
+		if ( file[0] === '.' || path.extname( file ) !== '.md' ) return;
 
-	const markdown = fs.readFileSync( `${root}/blog/${file}`, 'utf-8' );
+		const markdown = fs.readFileSync( `${root}/blog/${file}`, 'utf-8' );
 
-	const match = /---\n([\s\S]+?)\n---/.exec( markdown );
-	const frontMatter = match[1];
-	const content = markdown.slice( match[0].length );
+		const match = /---\n([\s\S]+?)\n---/.exec( markdown );
+		const frontMatter = match[1];
+		const content = markdown.slice( match[0].length );
 
-	const metadata = {};
-	frontMatter.split( '\n' ).forEach( pair => {
-		const colonIndex = pair.indexOf( ':' );
-		metadata[ pair.slice( 0, colonIndex ).trim() ] = pair.slice( colonIndex + 1 ).trim();
+		const metadata = {};
+		frontMatter.split( '\n' ).forEach( pair => {
+			const colonIndex = pair.indexOf( ':' );
+			metadata[ pair.slice( 0, colonIndex ).trim() ] = pair.slice( colonIndex + 1 ).trim();
+		});
+
+		const date = new Date( metadata.pubdate );
+		metadata.dateString = date.toDateString();
+
+		const html = marked( content.replace( /^\t+/gm, match => match.split( '\t' ).join( '  ' ) ) );
+
+		return {
+			html,
+			metadata,
+			slug: file.replace( /^[\d-]+/, '' ).replace( /\.md$/, '' )
+		};
+	})
+	.sort((a, b) => {
+		return a.metadata.pubdate < b.metadata.pubdate ? 1 : -1;
 	});
-
-	const date = new Date( metadata.pubdate );
-	metadata.dateString = date.toDateString();
-
-	const html = marked( content.replace( /^\t+/gm, match => match.split( '\t' ).join( '  ' ) ) );
-
-	return {
-		html,
-		metadata,
-		slug: file.replace( /^\d+-/, '' ).replace( /\.md$/, '' )
-	};
-});
 
 const preview = posts.map( post => {
 	return { slug: post.slug, metadata: post.metadata };
