1ec536488f6bf84a5331048227eddc04772a813b
Rich Harris <richard.a.harris@gmail.com>
2017-08-24 10:58:16 -0400

    remove package-lock.json from downloaded zip files

--- scripts/update_template.sh
+++ scripts/update_template.sh
@@ -1,3 +1,4 @@
+rm -rf scripts/svelte-app
 node_modules/.bin/degit sveltejs/template scripts/svelte-app -f
 rm -rf scripts/svelte-app/src
 cp scripts/svelte-app/public/global.css public/repl-viewer.css
