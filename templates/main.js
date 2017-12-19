417368665cbddfaef7fdb21e6906894bec37da6f
Rich Harris <richard.a.harris@gmail.com>
2017-12-19 16:38:57 -0500

    update sapper

--- templates/main.js
+++ templates/main.js
@@ -1,10 +1,3 @@
 import { init } from '__app__';
 
 init(document.querySelector('#sapper'), __routes__);
-
-// Sadly need to comment this out pending
-// https://github.com/sveltejs/sapper-template/issues/2
-// if (__dev__) {
-// 	import('sapper/webpack/hmr');
-// 	if (module.hot) module.hot.accept();
-// }
