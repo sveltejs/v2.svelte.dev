380dbb4c2e3bc1180f12ac816fafee5f8bfa7258
Rich Harris <richard.a.harris@gmail.com>
2017-12-20 20:59:52 -0500

    cache-first service worker

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const serve = require('serve-static');
 
 const { PORT = 3000 } = process.env;
 
+console.log(`initialising server`);
+
 const fetch = require('node-fetch');
 global.fetch = (url, opts) => {
 	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
@@ -19,6 +21,7 @@ app.use(sapper({
 	selector: '#sapper'
 }));
 
+console.log(`starting server on port ${PORT}`);
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
 });
