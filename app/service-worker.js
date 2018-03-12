ed6f3791fb14cdead7f870d50f114fa723633874
Rich Harris <richard.a.harris@gmail.com>
2018-03-11 20:44:35 -0400

    update deps

--- app/service-worker.js
+++ app/service-worker.js
@@ -51,6 +51,11 @@ self.addEventListener('fetch', event => {
 			.open(`offline${timestamp}`)
 			.then(async cache => {
 				try {
+					if (event.request.cache === 'only-if-cache') {
+						// workaround Chrome devtools bug https://github.com/sveltejs/sapper-template/issues/34
+						event.request.mode = 'same-origin';
+					}
+
 					const response = await fetch(event.request);
 					cache.put(event.request, response.clone());
 					return response;
