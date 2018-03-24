df7479d3e6b782a6d0884938ef311635d85d3111
Rich-Harris <richard.a.harris@gmail.com>
2018-03-24 12:40:58 -0400

    update to Sapper 0.10

--- app/server.js
+++ app/server.js
@@ -4,22 +4,12 @@ import sapper from 'sapper';
 import serve from 'serve-static';
 import { routes } from './manifest/server.js';
 
-const app = express();
-
-const { PORT = 3000 } = process.env;
-
-const fetch = require('node-fetch');
-global.fetch = (url, opts) => {
-	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
-	return fetch(url, opts);
-};
-
-app.use(compression({ threshold: 0 }));
-
-app.use(serve('assets'));
-
-app.use(sapper({
-	routes
-}));
-
-app.listen(PORT);
+express()
+	.use(
+		compression({ threshold: 0 }),
+		serve('assets'),
+		sapper({
+			routes
+		})
+	)
+	.listen(process.env.PORT);
--- routes/_store.js
+++ routes/_store.js
@@ -4,13 +4,12 @@ const store = new Store({
 	guide_contents: []
 });
 
-// TODO setTimeout is an ugly hack — this code runs before
-// global.fetch is set
-setTimeout(() => {
-	fetch(`/api/guide/contents`).then(r => r.json()).then(guide_contents => {
+// TODO set this in the sapper middleware
+if (process.browser) {
+	fetch(`api/guide/contents`).then(r => r.json()).then(guide_contents => {
 		store.set({ guide_contents });
 	});
-});
+}
 
 
 export default store;
