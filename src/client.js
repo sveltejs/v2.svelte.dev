09ed5e25314577e08d13d1d5984a9a05ca611ec6
Achim Vedam <vedam.art@gmail.com>
2018-09-30 19:30:45 +0200

    relink renamed toast

--- src/client.js
+++ src/client.js
@@ -3,30 +3,32 @@ import store from '../client/store.js';
 import { manifest } from './manifest/client.js';
 
 init({
-	target: document.querySelector('#sapper'),
-	manifest,
-	store: data => {
-		store.set(data);
+  target: document.querySelector('#sapper'),
+  manifest,
+  store: data => {
+    store.set(data);
 
-		fetch(`api/guide/contents`).then(r => r.json()).then(guide_contents => {
-			store.set({ guide_contents });
-		});
+    fetch(`api/guide/contents`)
+      .then(r => r.json())
+      .then(guide_contents => {
+        store.set({ guide_contents });
+      });
 
-		window.store = store;
-		return store;
-	}
+    window.store = store;
+    return store;
+  },
 });
 
 if (navigator.serviceWorker && navigator.serviceWorker.controller) {
-	navigator.serviceWorker.controller.onstatechange = function(e) {
-		if (e.target.state === 'redundant') {
-			import('./components/Toast.html').then(mod => {
-				mod.default.show();
-			});
-		}
-	};
+  navigator.serviceWorker.controller.onstatechange = function(e) {
+    if (e.target.state === 'redundant') {
+      import('./components/toast.html').then(mod => {
+        mod.default.show();
+      });
+    }
+  };
 }
 
 if (!localStorage.fonts) {
-	import('./utils/local-fonts.js');
+  import('./utils/local-fonts.js');
 }
