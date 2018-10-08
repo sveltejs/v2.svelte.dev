effe8eeb3f2788296eb0709dcc2e8723423cc107
Achim Vedam <vedam.art@gmail.com>
2018-10-08 18:41:22 +0200

    fixing nav

--- src/client.js
+++ src/client.js
@@ -2,10 +2,6 @@ import { init } from 'sapper/runtime.js';
 import store from '../client/store.js';
 import { manifest } from './manifest/client.js';
 
-// TODO
-// import CustomProperties from '../utils/css-custom-properties.js'
-// new CustomProperties()
-
 init({
   target: document.querySelector('#sapper'),
   manifest,
